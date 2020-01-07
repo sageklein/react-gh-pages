c531a54e760bdcc83ad5476d002d0179f758859f
Sage Klein <sageklein@gmail.com>
2020-01-06 21:43:56 -0600

    Updates

---
+++ precache-manifest.52c31d505e2049bbaf539060800b4ae5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a536804857c315e1c4b6aeb3e67e64f9",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "45eeb4a7945a7f0df34e",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a56f86182e1be4b73a34",
+    "url": "/react-gh-pages/static/js/2.dd64e209.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-gh-pages/static/js/2.dd64e209.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45eeb4a7945a7f0df34e",
+    "url": "/react-gh-pages/static/js/main.9c8fb20b.chunk.js"
+  },
+  {
+    "revision": "72af5fe30afbc208682e",
+    "url": "/react-gh-pages/static/js/runtime-main.e7ab8d6e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
